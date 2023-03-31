a1ae96d3cda577e073051b79c02af4b95685dcdf
汪東陽 <michael19920327@gmail.com>
2023-04-01 03:36:54 +0800

    feat: init server using fastify

---
+++ server/src/main.ts
@@ -0,0 +1,23 @@
+import createApp from 'fastify';
+
+export async function main() {
+  const app = createApp({
+    logger: {
+      transport: {
+        target: 'pino-pretty',
+      },
+    },
+  });
+
+  try {
+    await app.listen({
+      host: process.env.HOST || 'localhost',
+      port: +(process.env.PORT || 8080),
+    });
+  } catch (error: unknown) {
+    app.log.error(error);
+    process.exit(1);
+  }
+}
+
+main();
