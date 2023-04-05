a7319f5a3ff264650a5a1eb9a44b3a64670c63cd
汪東陽 <michael19920327@gmail.com>
2023-04-05 17:53:34 +0800

    feat: implement login logic

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,5 +1,6 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 
 import { createUser, findUserByEmail } from '../services'
 
@@ -23,15 +24,46 @@ export async function handleRegister (
   }
 
   // 3. hash password
-  const saltOrRounds = 10
-  const hashedPassword = await hash(password, saltOrRounds)
+  const saltRounds = 10
+  const hashedPassword = await hash(password, saltRounds)
 
   // 4. save user to database
   await createUser(email, hashedPassword)
 
-  reply.send({ message: 'User created' })
+  // 5. generate JWT
+  const token = sign({ email }, process.env.JWT_SECRET ?? 'secret')
+
+  reply.send({ token })
 }
 
-export async function handleLogin (request: FastifyRequest, reply: FastifyReply) {
-  // TODO: Implement
+export async function handleLogin (
+  request: FastifyRequest<{ Body: { email: string, password: string } }>,
+  reply: FastifyReply
+) {
+  const { email, password } = request.body
+
+  // 1. validate email and password
+  if (!email || !password) {
+    reply.badRequest('Email and password are required')
+    return
+  }
+
+  // 2. check if email is exists
+  const user = await findUserByEmail(email)
+  if (!user) {
+    reply.badRequest('Email is not registered')
+    return
+  }
+
+  // 3. compare password
+  const isPasswordCorrect = await compare(password, user.password)
+  if (!isPasswordCorrect) {
+    reply.unauthorized()
+    return
+  }
+
+  // 4. generate JWT
+  const token = sign({ email }, process.env.JWT_SECRET ?? 'secret')
+
+  reply.send({ token })
 }
