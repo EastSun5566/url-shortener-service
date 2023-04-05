885063974d507518d1b51fc00e5436f637b39983
汪東陽 <michael19920327@gmail.com>
2023-04-05 16:20:36 +0800

    feat: implement redirect logic

--- server/src/controllers/link.ts
+++ server/src/controllers/link.ts
@@ -1,7 +1,29 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { createShortenKey, getDbClient, setLinkCache } from '../services'
-export async function handleRedirect (request: FastifyRequest, reply: FastifyReply) {
-  // TODO: Implement redirect
+import { createShortenKey, getDbClient, getLinkCache, setLinkCache } from '../services'
+export async function handleRedirect (request: FastifyRequest<{ Params: { shortenKey: string } }>, reply: FastifyReply) {
+  const { shortenKey } = request.params
+
+  // 1. get from cache
+  const originalUrl = await getLinkCache(shortenKey)
+  if (originalUrl != null) {
+    reply.redirect(originalUrl)
+    return
+  }
+
+  // 2. get from database
+  const db = await getDbClient()
+  const link = await db.link.findUnique({
+    where: { shorten_key: shortenKey }
+  })
+  if (link === null) {
+    reply.status(404).send({ error: 'Link not found' })
+    return
+  }
+
+  // 3. add to cache
+  await setLinkCache(shortenKey, link.original_url)
+
+  reply.redirect(link.original_url)
 }
 
 export async function handleCreateLink (request: FastifyRequest<{ Body: { originalUrl: string } }>, reply: FastifyReply) {
@@ -28,5 +50,5 @@ export async function handleCreateLink (request: FastifyRequest<{ Body: { origin
   // 4. add to cache
   await setLinkCache(shortenKey, originalUrl)
 
-  reply.status(201)
+  reply.status(201).send({ shortenUrl: `http://localhost:8080/${shortenKey}` })
 }
