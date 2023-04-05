7bb927547f7a7be979d6824aa5ff7e2dff63e577
汪東陽 <michael19920327@gmail.com>
2023-04-05 20:20:21 +0800

    chore: add tailwind

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -14,17 +14,29 @@ export function Root (): JSX.Element {
 
   return (
     <main>
-      <h1>URL Shortener</h1>
+      <h1 className='mb-10'>URL Shortener</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form className='text-center mb-10' onSubmit={handleSubmit}>
         <input
+          className="w-full outline-none rounded-xl h-12 px-3 focus:ring-2 ring-indigo-400 ring-offset-2"
           type="url"
+          placeholder="Type URL here..."
           value={originalUrl}
           onChange={({ target }) => { setOriginalUrl(target.value) }}
         />
+
+        <button
+          className="px-5 h-12 rounded-xl bg-gradient-to-br from-indigo-600 to-indigo-700 text-white focus:ring-2 ring-indigo-400 ring-offset-2 disabled:opacity-50"
+          type="submit">
+            Shorten
+        </button>
       </form>
 
-      {shortenUrl && <a href={shortenUrl} target='_blank' rel="noreferrer">{shortenUrl}</a>}
+      {shortenUrl && (
+        <div className='text-center'>
+          <a href={shortenUrl} target='_blank' rel="noreferrer">{shortenUrl}</a>
+        </div>
+      )}
     </main>
   )
 }
